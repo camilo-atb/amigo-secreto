0a84f6ada967d83ef041157c074e996cf2e554f6
camilo-atb <camilotellez1821@gmail.com>
2025-01-23 20:29:14 -0500

    commit "Primer commit del juego amigo secreto"

---
+++ README.md
@@ -0,0 +1,21 @@
+# Desafío Amigo Secreto
+
+Este proyecto es parte de un desafío del programa **Oracle Next Education**, impartido por Oracle y Alura. El propósito principal de esta aplicación es reforzar los conceptos de **lógica de programación en JavaScript**, utilizando funciones, listas, métodos y algunas librerías. 
+
+## Descripción
+
+Este aplicativo permite al usuario agregar nombres a una lista, realizar un sorteo entre esos nombres y mostrar al ganador. El usuario puede escribir un nombre, añadirlo a la lista mediante el botón **"Añadir"** y realizar el sorteo presionando el botón **"Sortear amigo"**.
+
+### Requisitos:
+
+1. **Nombres únicos**: No se permiten nombres duplicados en la lista.
+2. **No se permiten cadenas vacías**: El campo de entrada no puede estar vacío.
+
+## Uso
+
+1. Ingresa los nombres de los participantes en el campo de texto.
+2. Haz clic en el botón **"Añadir"** para agregar un nombre a la lista.
+3. Para realizar el sorteo, haz clic en **"Sortear amigo"**.
+4. El nombre ganador será mostrado en pantalla
+
+¡Gracias por visitar este proyecto!
---
+++ app.js
@@ -0,0 +1,168 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// VARIABLES:
+let nombres = []; // Almacenamos los nombres
+
+// FUNCIONES: 
+
+function agregarAmigo(){
+    let nombreIngresado = document.getElementById("amigo").value; 
+    nombreIngresado = nombreIngresado.charAt(0).toUpperCase() + nombreIngresado.slice(1); // slice toma desde el indice en 1 en adelante, ya que debemos concatenar
+    if(nombreIngresado.trim() != "" && !nombres.includes(nombreIngresado)){ // .trim() elimina los espacios en blanco al principio y al final de una cadena de texto
+        nombres.push(nombreIngresado);
+        console.log(nombres);
+
+        // Mostrar nombres debajo de el input:
+        mostrarEnPagina(nombreIngresado); // con el metodo # 1
+        //mostrarListaPagina(nombreIngresado) // con el metodo # 2
+        
+    }else if(nombres.includes(nombreIngresado)){
+        alertify.alert(`Opps!!`, `El nombre "${nombreIngresado}" ya está en tu lista. ¡Recuerda no repetirlo!`);
+    }else if (nombreIngresado.trim() == ""){
+        // alert("Debes ingresar un nombre valido y que no hayas ingresado");
+        alertify.alert("Opps!!", "¡No puedes dejar este campo en blanco!");
+    }
+    // Limpiar el apartado de ingreso de datos:
+    limpiarInput();
+}
+// PRIMER METODO PARA MOSTRAR EN PANTALLA: 
+function mostrarEnPagina(nombre){
+    let textoLista = document.getElementById("listaAmigos"); 
+    let nuevoElemento = document.createElement("li"); 
+    let simbolo = document.createTextNode("🔶 "); //🔶▪️🔸
+    nuevoElemento.appendChild(simbolo);
+    nuevoElemento.appendChild(document.createTextNode(nombre)); 
+    textoLista.appendChild(nuevoElemento); 
+}
+
+// SEGUNDO METODO PARA MOSTRAR EN PANTALLA: 
+/*
+function mostrarListaPagina(){
+    let textoLista = document.getElementById("listaAmigos");
+    let nuevoElemento = document.createElement("li");
+    for(let i = 0; i < nombres.length; i++){
+        nuevoElemento.textContent = nombres[i];
+        textoLista.appendChild(nuevoElemento)
+    }
+}
+*/
+
+function limpiarInput(){
+    document.getElementById("amigo").value = "";
+}
+
+function sortearAmigo(){
+    if(nombres.length > 0){
+        // Indice Aleatorio
+        const indiceAleatorio = Math.floor(Math.random() * nombres.length);
+        
+        // Mostrar Ganador:
+        let textoGanador = document.querySelector(".section-title"); 
+        textoGanador.innerHTML = `LA PERSONA GANADORA ES: ${(nombres[indiceAleatorio]).toUpperCase()}`
+        
+        // Llamar la Función de Celebración 
+        celebrarGanador();   
+    }else{
+        // alert("Debes ingresar nombres a la lista antes de realizar el sorteo");
+        alertify.alert("Opps!!", "Debes ingresar nombres a la lista antes de realizar el sorteo");
+    }
+}
+
+// CELEBRACIÓN confetti
+
+
+function celebrarGanador() {
+    confetti({
+        spread: 360,
+        ticks: 100, /* Duración */
+        gravity: 1,
+        decay: 0.94,
+        startVelocity: 30,
+        particleCount: 400,
+        scalar: 3,
+        shapes: ["image"],
+        shapeOptions: {
+          image: [{
+              src: "https://particles.js.org/images/fruits/apple.png",
+              width: 32,
+              height: 32,
+            },
+            {
+              src: "https://particles.js.org/images/fruits/avocado.png",
+              width: 32,
+              height: 32,
+            },
+            {
+              src: "https://particles.js.org/images/fruits/banana.png",
+              width: 32,
+              height: 32,
+            },
+            {
+              src: "https://particles.js.org/images/fruits/berries.png",
+              width: 32,
+              height: 32,
+            },
+            {
+              src: "https://particles.js.org/images/fruits/cherry.png",
+              width: 32,
+              height: 32,
+            },
+            {
+              src: "https://particles.js.org/images/fruits/grapes.png",
+              width: 32,
+              height: 32,
+            },
+            {
+              src: "https://particles.js.org/images/fruits/lemon.png",
+              width: 32,
+              height: 32,
+            },
+            {
+              src: "https://particles.js.org/images/fruits/orange.png",
+              width: 32,
+              height: 32,
+            },
+            {
+              src: "https://particles.js.org/images/fruits/peach.png",
+              width: 32,
+              height: 32,
+            },
+            {
+              src: "https://particles.js.org/images/fruits/pear.png",
+              width: 32,
+              height: 32,
+            },
+            {
+              src: "https://particles.js.org/images/fruits/pepper.png",
+              width: 32,
+              height: 32,
+            },
+            {
+              src: "https://particles.js.org/images/fruits/plum.png",
+              width: 32,
+              height: 32,
+            },
+            {
+              src: "https://particles.js.org/images/fruits/star.png",
+              width: 32,
+              height: 32,
+            },
+            {
+              src: "https://particles.js.org/images/fruits/strawberry.png",
+              width: 32,
+              height: 32,
+            },
+            {
+              src: "https://particles.js.org/images/fruits/watermelon.png",
+              width: 32,
+              height: 32,
+            },
+            {
+              src: "https://particles.js.org/images/fruits/watermelon_slice.png",
+              width: 32,
+              height: 32,
+            },
+          ],
+        },
+      });
+}
